9c1ff5660cca219fccd62fc5c48e22ed595153cd
208757242 <208757242@qq.com>
2025-06-18 23:10:54 +0800

    修改中继服务器域名和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["114.55.173.146"];
+pub const RS_PUB_KEY: &str = "XnIjkVgKDFcHq8aAhkEDLc3okEX6CSkHopVLB+j2sh8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
